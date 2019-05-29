a3726d46012e58a34f39509dceab33532909c624
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-05-28 20:30:09 -0400

    Create TestStopWatch.java

---
+++ Stopwatch/TestStopWatch.java
@@ -0,0 +1,24 @@
+package stopwatch;
+
+import java.util.Random;
+
+public class TestStopWatch {
+	public static void main(String[] args) {
+		Stopwatch Time = new Stopwatch();
+		int [] testArray = new int [100000];
+		System.out.println("Fill array will 100000 random numbers.");
+		Time.start();
+		//System.out.println(Time.start());
+		fillArray(testArray);
+		Time.stop();
+		//System.out.println(Time.stop());
+		System.out.printf("\nElapsed time: %01d millisecond(s)", Time.getElapsedTime());
+	}
+	// method to fill array
+	public static void fillArray(int testArray[]) {
+		Random num = new Random();
+		for(int i = 0; i < testArray.length; i++) {
+			testArray[i] = num.nextInt();
+		}
+	}
+}
