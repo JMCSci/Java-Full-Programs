caea70645d61ab5f411e4e2595842b85379aa3b0
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-06-07 19:08:39 -0400

    Update ATM.java

--- ATM Machine/ATM.java
+++ ATM Machine/ATM.java
@@ -3,7 +3,7 @@
  * Accounts (ID #'s) are numbered 0 thru 9
  * The PIN number for all accounts is 1234
  * Initial balance in accounts is $100
- * ATM loop never ends; prompts a new user to enter their "account" (ID)
+ * ATM loop never ends; prompts a new user to enter their "account" (ID)  
  */
 
 package account;
@@ -23,21 +23,21 @@ public class ATM {
 	// fill ID array
 	for(int i = 0; i < bankAccountsID.length; i++) {
 			bankAccountsID[i]= i;
- 		}
+ 	}
 	// fill Balance array 
 	for(int i = 0; i < bankAccountsBal.length; i++) {
 		bankAccountsBal[i] = 100;
 	}
 	
 	// prompt user to enter ID number; if number does match "account #", asks user to enter correct number 
-	System.out.print("Enter an id: ");
+	System.out.print("Enter an ID: ");
 	atm.setId(input.nextInt());
 	
 	// check ID number
 	while(true) {
 		if(atm.getId() < 0 || atm.getId() >= 10) {
-		System.out.println("Number is incorrect. Please enter correct number. "); 
-		System.out.print("Enter an id: ");
+		System.out.println("ID is incorrect. Please enter correct number. "); 
+		System.out.print("Enter an ID: ");
 		atm.setId(input.nextInt());
 		continue;
 	  } 
@@ -61,7 +61,7 @@ public class ATM {
 				System.out.println("PIN is incorrect. Try again.");
 				PIN = input.nextInt();
 		}
-		System.out.println("\n***Main Menu***");
+		System.out.println("\n*** Main Menu ***");
 		System.out.println("1. Check balance");
 		System.out.println("2. Withdraw");
 		System.out.println("3. Deposit");
@@ -72,7 +72,7 @@ public class ATM {
 		case 1: System.out.println("Balance: $" + bankAccountsBal[atm.getId()]); break;
 		case 2: System.out.println("Enter an amount to withdraw: ");
 				bankAccountsBal[atm.getId()] = atm.withdraw(input.nextDouble());
-				System.out.println("New balance: $" + bankAccountsBal[atm.getId()]); break;
+				System.out.println("Balance: $" + bankAccountsBal[atm.getId()]); break;
 		case 3: System.out.println("Enter an amount to deposit: ");
 				bankAccountsBal[atm.getId()] = atm.deposit(input.nextDouble());
 				System.out.println("New balance: $" + bankAccountsBal[atm.getId()]); break;
@@ -80,14 +80,14 @@ public class ATM {
 		default: System.out.println("Invalid selection."); break;
 		}
 		// start new loop
-		System.out.println("\nEnter an id: ");
+		System.out.println("\nEnter an ID: ");
 		atm.setId(input.nextInt());
 		
 		// check ID number 
 		while(true) {
 			if(atm.getId() < 0 || atm.getId() >= 10) {
-			System.out.println("Number is incorrect. Please enter correct number. "); 
-			System.out.print("Enter an id: ");
+			System.out.println("ID is incorrect. Please enter correct number. "); 
+			System.out.print("Enter an ID: ");
 			atm.setId(input.nextInt());
 			continue;
 		  }  else {
