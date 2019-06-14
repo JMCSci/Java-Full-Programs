e08da40d3f3f0ab97836aaa6195e7048871fd330
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-06-14 17:02:17 -0400

    Update TestDryer.java

--- Commerical Dryer/TestDryer.java
+++ Commerical Dryer/TestDryer.java
@@ -26,9 +26,9 @@ public class DryerTest {
 		case "YES" : dryer.startCycle(); 	
 					 System.out.printf("Balance: $%.2f\n", dryer.getCardValue());
 					 dryer.endTime();				
-				     System.out.print("\nSelect dryer temperature: \nLOW (1), MEDIUM (2), HIGH (3): ");
-					 dryer.dryerTemperature(input.nextInt()); 	     break;
-		case "NO"  : System.out.println("*** END OF PROGRAM ***");   break;	
+				    	 System.out.print("\nSelect dryer temperature: \nLOW (1), MEDIUM (2), HIGH (3): ");
+					 dryer.dryerTemperature(input.nextInt()); 	 break;
+		case "NO"  : System.out.println("*** END OF PROGRAM ***");  	   	 break;	
 		default    : System.out.println("Invalid response.");
 					 System.out.println("\n*** END OF PROGRAM ***"); break;	
 		}
@@ -36,25 +36,25 @@ public class DryerTest {
 		while(dryer.dryerStatus() == 1) {
 			
 		// loop to check if cycle is complete
-			while(dryer.checkCycle() == true && dryer.getStatus() == 1) {	
+		while(dryer.checkCycle() == true && dryer.getStatus() == 1) {	
 				
 		// display card balance
-				System.out.printf("Balance: $%.2f\n", dryer.getCardValue());
+			System.out.printf("Balance: $%.2f\n", dryer.getCardValue());
 				
 		// add time to cycle
-				System.out.println("\nWould you like to add an additional 5 seconds for 1.00?");
-				System.out.print("Type YES or NO: ");
-				choice = input.next();
-				choice = choice.toUpperCase();
+			System.out.println("\nWould you like to add an additional 5 seconds for 1.00?");
+			System.out.print("Type YES or NO: ");
+			choice = input.next();
+			choice = choice.toUpperCase();
 		
-				switch(choice) {
+			switch(choice) {
 				case "YES" : dryer.addCycleTime(); break;
 				case "NO"  : dryer.checkAddTime(); break;
 				default    : System.out.println("\n\n*** Invalid response ***");
 				}
 				
 		// change dryer status based on time (ON or OFF)
-				dryer.dryerStatus();
+			dryer.dryerStatus();
 		}
 		
 	 }	
