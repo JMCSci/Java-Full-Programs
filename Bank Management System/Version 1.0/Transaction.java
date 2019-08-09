153cde1ae821654558d73efb674b410b3b5764dd
Jason M <40440123+JMCSci@users.noreply.github.com>
2019-08-08 21:02:12 -0400

    Add Transaction.java to Bank Management System

---
+++ Bank Management System/Version 1.0/Transaction.java
@@ -0,0 +1,76 @@
+/* Transaction class
+ */
+
+package bank;
+
+public class Transaction extends Account {
+	private char type;									// type of transaction ('W' for withdrawal, 'D' for deposit)
+	private double amount; 								// amount of this transaction
+	private double balance;								// new balance after this transaction
+	private String description;							// description of this transcription
+	
+// no-arg constructor
+	Transaction() {
+		
+	}
+	
+// argument constructor -- specified data
+	Transaction(char type, double amount, double balance, String description){
+		this.type = type;
+		this.amount = amount;
+		this.balance = balance;
+		this.description = description;
+		dateCreated();
+	}
+	
+// getters
+	public char getType() {
+		return type;
+	}
+	
+	public double getAmount() {
+		return amount;
+	}
+	
+	public double getNewBalance() {
+		if(type == 'W') {
+		return balance = balance - getAmount();
+		} else if(type == 'D') {
+			return balance = balance + getAmount();
+		} else if(type == 'T') {
+			return balance = balance - getAmount();
+		}
+		return 0;	
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+// setters
+	public void setType(char newValue) {
+		type = newValue;
+	}
+	
+	public void setAmount(double newValue) {
+		amount = newValue;
+	}
+	
+	public void setNewBalance(double newValue) {
+		balance = newValue;
+	}
+	
+	public void setDescription(String newValue) {
+		description = newValue;
+	}
+	
+	@Override
+	public String toString() {
+		String x = "\nType: " + getType() + "\nAmount: " + getAmount() + "\nBalance: " + getNewBalance() +
+				"\nDescription: " + getDescription() + "\nDate: " + dateCreated() + "\n";
+		
+		return x;
+					
+	}
+	
+}
