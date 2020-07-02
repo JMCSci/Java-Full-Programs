6fb9c160480ca6546ef70980f6bab57090422108
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-07-02 17:01:44 -0400

    Update ProgressBar.java
    - Add colon to "Progress"

--- Instagram Photo Downloader/Command Line/ProgressBar.java
+++ Instagram Photo Downloader/Command Line/ProgressBar.java
@@ -16,22 +16,22 @@ public class ProgressBar {
 				for(int i = 1; i < 12; i++) {
 					s = s.replace(i, i + 1, "\u2588");
 				}
-				System.out.print("Progress " + s + " 25% \r");
+				System.out.print("Progress: " + s + " 25% \r");
 			} else if(count >= len * 0.25 && count <= len * 0.50) {
 				for(int i = 1; i < 25; i++) {
 					s = s.replace(i, i + 1, "\u2588");
 				}
-				System.out.print("Progress " + s + " 50% \r");
+				System.out.print("Progress: " + s + " 50% \r");
 			} else if(count >= len * 0.50 && count <= len * 0.75) {
 				for(int i = 1; i < 37; i++) {
 					s = s.replace(i, i + 1, "\u2588");
 				}
-				System.out.print("Progress " + s + " 75% \r");
+				System.out.print("Progress: " + s + " 75% \r");
 			} else {
 				for(int i = 1; i < s.length() - 1; i++) {
 					s = s.replace(i, i + 1, "\u2588");
 				}
-				System.out.print("Progress " + s + " 100% \r");
+				System.out.print("Progress: " + s + " 100% \r");
 			}
 	}
 
