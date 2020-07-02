86562e701e7baf7269217bb32679faa4a36b74bf
Jason M <40440123+JMCSci@users.noreply.github.com>
2020-07-02 16:49:09 -0400

    Update ProgressBar.java
    - Changed bar graphic to unicode block

--- Instagram Photo Downloader/Command Line/ProgressBar.java
+++ Instagram Photo Downloader/Command Line/ProgressBar.java
@@ -14,25 +14,24 @@ public class ProgressBar {
 		StringBuilder s = new StringBuilder("[                                                  ]");
 			if(count <= len * 0.25) {
 				for(int i = 1; i < 12; i++) {
-					s = s.replace(i, i + 1, "=");
+					s = s.replace(i, i + 1, "\u2588");
 				}
-				System.out.print(s + " 25% \r");
+				System.out.print("Progress " + s + " 25% \r");
 			} else if(count >= len * 0.25 && count <= len * 0.50) {
 				for(int i = 1; i < 25; i++) {
-					s = s.replace(i, i + 1, "=");
+					s = s.replace(i, i + 1, "\u2588");
 				}
-				System.out.print(s + " 50% \r");
+				System.out.print("Progress " + s + " 50% \r");
 			} else if(count >= len * 0.50 && count <= len * 0.75) {
 				for(int i = 1; i < 37; i++) {
-					s = s.replace(i, i + 1, "=");
+					s = s.replace(i, i + 1, "\u2588");
 				}
-				System.out.print(s + " 75% \r");
-				
+				System.out.print("Progress " + s + " 75% \r");
 			} else {
 				for(int i = 1; i < s.length() - 1; i++) {
-					s = s.replace(i, i + 1, "=");
+					s = s.replace(i, i + 1, "\u2588");
 				}
-				System.out.print(s + " 100% \r");
+				System.out.print("Progress " + s + " 100% \r");
 			}
 	}
 
